55432226db726a874cbe8142af263a000033df93
ahmadmunzir <ahmadmunzirmohamad@gmail.com>
2022-07-21 18:00:02 +0800

    Update pengguna.blade.php

--- resources/views/pengguna.blade.php
+++ resources/views/pengguna.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @push('css')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <link href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet" >
 
 @endpush
@@ -30,13 +30,12 @@
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-
             <div class="contrainer">
                 <button type="button" class="btn btn-success" onclick="window.location.href='/tambahpengguna'">Tambah +</button>
                 <br/>
                 <br/>
                 <div class="row">
-                    <table class="table table-bordered" id="table_pengguna">
+                    <table class="table table-hover text-nowrap" id="table_pengguna">
                         <thead>
                             <tr>
                             <th scope="col">#</th>
@@ -82,25 +81,24 @@
 
 
         </div><!-- /.container-fluid -->
-        </section>
+    </section>
         <!-- /.content -->
-    </div>
+</div>
 
     @endsection
 
     @push('scripts')
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-            <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+
             <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+            <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
             </body>
             <script>
-              $(document).ready( function () {
-                $('#table_pengguna').DataTable();
-            } );
+            $('#table_pengguna').DataTable();
             $(".delete").click( function(){
                 var pengguna_id = $(this).attr('data-id');
 
