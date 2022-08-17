5d7d49a64a6a5559ea290a8a063ebca9058dfaef
tarmizirahim09 <tarmizi@map2u.com.my>
2022-08-17 12:26:58 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::group(['middleware' => ['auth','AccessLevel:1,99']], function(){
     Route::get('/mapviewer', [PenggunaController::class, 'mapviewer'] )->name('mapviewer');
 });
 
-//test
+//testmizi
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
