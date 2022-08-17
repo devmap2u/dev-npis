f16913eca39ac72482d37e4276c92b2098b916c0
devnpis <ahmadmunzirmohamad@gmail.com>
2022-08-17 12:57:09 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::group(['middleware' => ['auth','AccessLevel:1,99']], function(){
 });
 
 
-//tester123
+//tester123s
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
